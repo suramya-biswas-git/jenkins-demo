8ec6366037175a30d8a97501858f5cde29d7d4a7
suramya-biswas-git <suramya.biswas@gmail.com>
2026-04-27 21:34:45 +0530

    This is the 2nd version

--- JenkinsJavaApp.java
+++ JenkinsJavaApp.java
@@ -2,6 +2,7 @@ public class JenkinsJavaApp{
    public static void main(String [] str){
      System.out.println("Hello, It is a Java with Jenkins Application");
      System.out.println("Hi, It is a Java with Jenkins Application");
+   System.out.println("So I added a new line");
 
     }
 }
